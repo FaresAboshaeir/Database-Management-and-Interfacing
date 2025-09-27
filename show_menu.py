96a914ef668ecaa8c0a5d3098ebcc5a7346a1035
Fares Abo-Shaeir <154484077+FaresAboshaeir@users.noreply.github.com>
2025-09-27 17:08:41 -0400

    Create show_menu.py

---
+++ show_menu.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# show_menu.py
+# Purpose: Display a menu with 4 items
+# Author: Fares abos0008
+# Date: September 27, 2025
+# Version: 1.0
+# Completion time: 10m
+
+def show_menu():
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    return
+
+# Main program
+show_menu()
