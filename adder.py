88b3264bfcacc0c70333d2b82593ac4d70442ff6
Fares Abo-Shaeir <154484077+FaresAboshaeir@users.noreply.github.com>
2025-09-27 17:09:53 -0400

    Create adder.py

---
+++ adder.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# adder.py
+# Purpose: Add two numbers using a function
+# Author: Fares abos0008
+# Date: September 27, 2025
+# Version: 1.0
+# Completion time: 10m
+
+def add(number_1, number_2):
+    return number_1 + number_2
+
+# Main program
+n1 = float(input("Enter first number: "))
+n2 = float(input("Enter second number: "))
+print("Result:", add(n1, n2))
